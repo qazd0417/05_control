b9dc8bb7ff9082d8fabbb5fd213df4f558dea6d7
qazd0417 <70630107+qazd0417@users.noreply.github.com>
2020-10-06 16:49:26 +0900

    ysj05_05

--- main.c
+++ main.c
@@ -4,14 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum=0;
-	int a, i;
-
-	printf("input a number:");
-	scanf("%d",&a);
-	
-	for (i=0; i<=a; i++)
-		sum += i;
-	printf("the sum result is %d\n",sum);
+	int answer=59;
+	int a;
+	int b=0;
+	do{
+		printf("숫자를 입력해 주세요.:");
+		scanf("%d",&a);
+		if(a>answer){
+		
+			printf("low!\n");
+		}
+		else if(a<answer){
+		
+			printf("high!\n");
+		}
+		b++;
+		
+	}while(a!=answer);
+		printf("congratulation, number of trial:%d\n",b);
 	return 0;
 }
